cb5ea4de7e8eac10b158eab8d590cc0c38c3c852
mayunseo <skuop4697@gmail.com>
2023-06-06 22:05:10 +0900

    Add get_reviews

--- collector/collect_movie_review.py
+++ collector/collect_movie_review.py
@@ -1,3 +1,4 @@
+import requests
 import re
 import math
 import time
@@ -7,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from db.movie_crud import add_review
 
 # anaconda prompt
 # > conda activate cnu_python
@@ -29,10 +31,10 @@ driver = webdriver.Chrome(service=driver, options=options)
 
 # selenium : 동적 페이지에서 웹 크롤링 가능!
 #             -> 원래 용도 : 웹 브라우저 테스트용
-# http: 웹
-# ftp: 파일 전송
-# ssh: 터미널 접속
-# smtp: 메일 전송
+# http : 웹
+# ftp : 파일 전송
+# ssh : 터미널 접속
+# smtp : 메일 전송
 
 ##############################
 # 2.Open URL in ChromeDriver #
@@ -44,13 +46,13 @@ url = f"https://movie.daum.net/moviedb/grade?movieId={movie_id}"
 driver.get(url)
 time.sleep(1)  # 1초 딜레이(웹 페이지 랜더링 완료까지 기다리기)
 doc_html = driver.page_source  # 해당 URL 소스코드
-print(doc_html)
+
 doc = BeautifulSoup(doc_html, "html.parser")
 title = doc.select("span.txt_tit")[1].text.strip()  # 영화 제목 수집
 
 # 전체 리뷰 : 91개
 # 기존 출력 : 10개
-# 1클릭 추가 리뷰 : 30개 추가
+# 1클릭 추가 리뷰 : 30개
 # (전체 리뷰 - 기존 출력) / 30 = 3 (평점 더보기 클릭 횟수)
 
 total_review_txt = doc.select("span.txt_netizen")[0].text
@@ -64,22 +66,46 @@ for i in range(click_cnt):
     time.sleep(1)
 time.sleep(5)
 
-# >> 해당 페이지에 모든 리뷰 출력 완료
+# 해당 페이지에 모든 리뷰 출력 완료
 review_html = driver.page_source
 doc = BeautifulSoup(review_html, "html.parser")
 review_list = doc.select("ul.list_comment div.cmt_info")
 
-for review_box in review_list:
+empty_cnt = 0
+empty_list = []
+for i, review_box in enumerate(review_list):
+    if len(review_box.select("p.desc_txt")) == 0:
+        empty_cnt += 1  # empty review cnt+1
+        empty_list.append(i+1)  # empty review number
+    else:
+        review = review_box.select("p.desc_txt")[0].text.strip()
     score = review_box.select("div.ratings")[0].text
-    writer = review_box.select("a.link_nick")[0].text
+    writer = review_box.select("a.link_nick > span")[1].text
     review_date = review_box.select("span.txt_date")[0].text
-    review = review_box.select("p.desc_txt")[0].text
-    if writer != '댓글작성자':
-        print(list.remove(("댓글작성자")))
-    if score != '댓글모아보기':
-        list.remove("댓글모아보기")
+    print(f"={i+1}=======================================================")
     print(f" - 평점 : {score}")
     print(f" - 작성자 : {writer}")
     print(f" - 작성일자 : {review_date}")
     print(f" - 리뷰 : {review}")
-    # 숙제:리뷰, 작성자, 작성일자 수집(평점은 해주심)
+
+# MongoDB 저장
+#  - JSON type(Dict) 전달
+data = {
+    "title" : title,
+    "review" : review,
+    "score" : score,
+    "writer" : writer,
+    "regdate" : review_date
+}
+add_review(data)
+
+# Report
+print("=" * 30)
+print(f"# MOVIE TITLE : {title}")
+print("#" * 30)
+print(f"  - Total Review : {total_review}")
+print(f"  - Empty Review : {empty_cnt}")
+if len(empty_list) > 0:
+    print(f"    + {empty_list}")
+print(f"  - Reviews collected : {total_review - empty_cnt}")
+print("#" * 30)
